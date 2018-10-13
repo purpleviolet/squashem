2ea42be3408814cc2846c599a636ba6214d06bbe
Diane Chen <dianedc@purpleweb.com>
2018-10-13 09:38:54 -0700

    Initial code for squashem

--- squashem/squashem.py
+++ squashem/squashem.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+
+def squashem(matrix):
+    """Return squashed list from 2D list of lists"""
+    return [
+        item
+        for row in matrix
+        for item in row
+    ]
